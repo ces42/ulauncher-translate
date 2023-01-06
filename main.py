128c7845e949162dffd6f2b3055f8160b081322f
Carlos Esparza <ca2010es@gmail.com>
2023-01-06 14:34:28 +0100

    Big rewrite - Switched from accessing google translate via http to accessing the   google translate api via [`googletrans`](https://github.com/ssut/py-googletrans) - show languages (for auto language detection) - show pronunciation (as part of result) - some refactoring

--- main.py
+++ main.py
@@ -11,60 +11,32 @@ from ulauncher.api.shared.action.CopyToClipboardAction import CopyToClipboardAct
 import textwrap
 import sys
 import re
+from googletrans import Translator
 
-if (sys.version_info[0] < 3):
-    import urllib2
-    import urllib
-    import HTMLParser
-else:
-    import html
-    import urllib.request
-    import urllib.parse
+import time # todo: remove
 
-agent = {'User-Agent': "Mozilla/5.0 (Android 9; Mobile; rv:67.0.3) Gecko/67.0.3 Firefox/67.0.3"}
-
-
-def unescape(text):
-    if (sys.version_info[0] < 3):
-        parser = HTMLParser.HTMLParser()
-    else:
-        parser = html
-    return (parser.unescape(text))
-
-
-def translate(to_translate, to_language="auto", from_language="auto", wrap_len="80"):
-    base_link = "http://translate.google.com/m?tl=%s&sl=%s&q=%s"
-    if (sys.version_info[0] < 3):
-        to_translate = urllib.quote_plus(to_translate)
-        link = base_link % (to_language, from_language, to_translate)
-        request = urllib2.Request(link, headers=agent)
-        raw_data = urllib2.urlopen(request).read()
-    else:
-        to_translate = urllib.parse.quote(to_translate)
-        link = base_link % (to_language, from_language, to_translate)
-        request = urllib.request.Request(link, headers=agent)
-        raw_data = urllib.request.urlopen(request).read()
-       
-    data = raw_data.decode("utf-8")
-    
-    expr = r'class="result-container">(.*?)<'
-    re_result = re.findall(expr, data)
-    if (len(re_result) == 0):
-        result = ""
-    else:
-        result = unescape(re_result[0])
-    
-    return ("\n".join(textwrap.wrap(result, int(wrap_len) if wrap_len.isdigit() else 80 )))
-
+AGENT =  "Mozilla/5.0 (Android 9; Mobile; rv:67.0.3) Gecko/67.0.3 Firefox/67.0.3"
 
 
 class TranslateExtension(Extension):
+    translator: Translator
+
     def __init__(self):
         super(TranslateExtension, self).__init__()
-        self.subscribe(KeywordQueryEvent, KeywordQueryEventListener())
+        self.translator = Translator(user_agent=AGENT)
+        self.subscribe(KeywordQueryEvent, KeywordQueryEventListener(self.translate))
+    
+    def translate(self, query, to_language="auto", from_language="auto"):
+        result = self.translator.translate(query, src=from_language, dest=to_language)
+        # res_text, orig, to = result.text, result.src, result.dest
+        # return res_text, orig, to
+        return result.text, result.src, result.dest, result.pronunciation
 
 
 class KeywordQueryEventListener(EventListener):
+    def __init__(self, tr_func):
+        self.tr_func = tr_func
+
     def on_event(self, event, extension):
         query = event.get_argument() or str()
         
@@ -78,21 +50,34 @@ class KeywordQueryEventListener(EventListener):
         if len(query)>3 and ":" in query[0]:
             from_language = "auto"
             to_language = query[1:3]
-            query = query[3:]
+            query = query[3:].strip()
         elif len(query)>5 and ":" in query[2]:
             from_language = query[:2]
             to_language = query[3:5]
-            query = query[5:]
+            query = query[5:].strip()
         else:
             from_language = extension.preferences["otherlang"]
             to_language = extension.preferences["mainlang"]
-        ceviri = translate(query, to_language, from_language, extension.preferences["wrap"])
+        
+        if to_language == 'zh':
+            to_language = 'zh-CN'
+
+        result, orig, to, pronunc = self.tr_func(query, to_language, from_language)
+        try:
+            wrap_len = int(extension.preferences['wrap'])
+        except ValueError:
+            wrap_len = 80
+        
+        if pronunc and pronunc != result and len(pronunc) + len(result) + 4 <= wrap_len:
+            res_text = f'{result}  "{pronunc}"'
+        else:
+            res_text = '\n'.join(textwrap.wrap(result, wrap_len))
         
         items = [
             ExtensionResultItem(icon='images/icon.png',
-                                name=query.replace("\n",""),
-                                description=translate(query, to_language, from_language, extension.preferences["wrap"]),
-                                on_enter=CopyToClipboardAction(ceviri))
+                                name=query.replace("\n","") + f'  [{orig} → {to}]',
+                                description=res_text,
+                                on_enter=CopyToClipboardAction(result))
         ]
 
         return RenderResultListAction(items)
